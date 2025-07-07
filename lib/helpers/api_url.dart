d59e61f735fde2cde417ecbad9f575df172f774e
jefriwahyu <memberjefry1@gmail.com>
2025-07-07 12:54:36 +0700

    merapikan kode api

--- lib/helpers/api_url.dart
+++ lib/helpers/api_url.dart
@@ -1,25 +1,24 @@
 // Enum untuk memilih jenis API
 enum ApiType { express, codeigniter }
 
-// --- GANTI DI SINI UNTUK MEMILIH BACKEND ---
 // Ganti ke ApiType.codeigniter jika ingin menguji backend CI4
 const ApiType currentApi = ApiType.codeigniter;
 // -----------------------------------------
 
 class ApiUrl {
-  // 1. Definisikan base URL murni (tanpa path)
+  //Definisikan base URL murni (tanpa path)
   static const String _expressBase = 'http://localhost:3000'; // Sesuaikan port
   static const String _ci4Base =
       'https://kontak-api.tinagers.com'; // Sesuaikan port
 
-  // 2. Definisikan path lengkap untuk setiap endpoint
+  //Definisikan path lengkap untuk setiap endpoint
   static const String _expressContactsPath = '/api/kontak';
   static const String _expressUploadPath = '/api/kontak/upload';
 
   static const String _ci4ContactsPath = '/api/kontak';
   static const String _ci4UploadPath = '/api/upload';
 
-  // 3. Buat getter dinamis untuk mendapatkan URL yang benar
+  //Buat getter dinamis untuk mendapatkan URL yang benar
   static String get contactsUrl {
     if (currentApi == ApiType.express) {
       return _expressBase + _expressContactsPath;
