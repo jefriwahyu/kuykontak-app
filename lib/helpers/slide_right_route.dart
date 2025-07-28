a16bc274a7ec0fc6c222873e68fc457a32d33629
jefriwahyu <memberjefry1@gmail.com>
2025-07-28 21:30:49 +0700

    memperbaiki slide_right

--- lib/helpers/slide_right_route.dart
+++ lib/helpers/slide_right_route.dart
@@ -1,38 +1,40 @@
 import 'package:flutter/material.dart';
 
-// Kelas ini akan kita gunakan untuk menggantikan MaterialPageRoute
+/// [SlideRightRoute] adalah kelas untuk membuat animasi transisi halaman
 class SlideRightRoute extends PageRouteBuilder {
   final Widget page;
 
   SlideRightRoute({required this.page})
       : super(
-          // Durasi animasi
+          // Durasi total animasi transisi.
           transitionDuration: const Duration(milliseconds: 300),
-          // Halaman tujuan
+
+          // Builder untuk halaman tujuan yang akan ditampilkan setelah transisi.
           pageBuilder: (
             BuildContext context,
             Animation<double> animation,
             Animation<double> secondaryAnimation,
           ) =>
               page,
-          // Builder untuk membuat transisi animasinya
+
+          // Builder yang mendefinisikan dan membuat widget animasi transisi.
           transitionsBuilder: (
             BuildContext context,
             Animation<double> animation,
             Animation<double> secondaryAnimation,
             Widget child,
           ) {
-            // Tentukan posisi awal dan akhir
-            const begin = Offset(1.0, 0.0); // Mulai dari kanan luar layar
-            const end = Offset.zero; // Berakhir di tengah layar
-            // Tentukan kurva animasi (misal: ease-out)
-            const curve = Curves.easeOut;
+            // Tentukan posisi awal dan akhir transisi.
+            const begin =
+                Offset(1.0, 0.0); // Mulai dari kanan (100% lebar layar).
+            const end = Offset.zero; // Berakhir di posisi normal (0,0).
+            const curve = Curves.easeOut; // Kurva untuk memperhalus animasi.
 
-            // Gabungkan tween dengan kurva
+            // Gabungkan posisi (Tween) dengan kurva animasi.
             var tween =
                 Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-            // Gunakan SlideTransition untuk menganimasikan perpindahan halaman
+            // Gunakan SlideTransition untuk menganimasikan perpindahan halaman.
             return SlideTransition(
               position: animation.drive(tween),
               child: child,
